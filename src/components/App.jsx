b6599b4e117cc54360f35cd282c9c00768a1a5a6
Divy Agrawal <divyagrawal29@gmail.com>
2023-11-24 14:52:15 +0530

    Formatting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import emojipedia from "../emojipedia";
 import Entry from "../Entry";
-// import emojipedia from './../emojipedia';
 
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      meaning={emojiTerm.meaning}
-    />
-  );
-}
+// function createEntry(emojiTerm) {
+//   return (
+//     <Entry
+//       key={emojiTerm.id}
+//       emoji={emojiTerm.emoji}
+//       name={emojiTerm.name}
+//       meaning={emojiTerm.meaning}
+//     />
+//   );
+// }
 
 function App() {
   return (
@@ -21,7 +20,18 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {/* {emojipedia.map(createEntry)}*/}
+
+        {emojipedia.map((emojiTerm) => (
+          <Entry
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            meaning={emojiTerm.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
